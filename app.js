6f842c3f8d68d9c0242b67b196fa40f29734f094
umairaslam2 <154333213+umairaslam2@users.noreply.github.com>
2024-04-15 20:41:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+//""
+//g
+//h
+
+
+//Chapter 17-20
+
+//question 3;
+
+// var i;
+
+// for ( i= 1; i<=10; i++) {
+//    document.write( i+ "<br>")
+    
+// }
+
+
+//question 5;
+
+// var cities =["apple", "banana", "mango", "orange", "strawberry"];
+
+
+//  for (var i =0; i <=cities.length; i++) {
+//      console.log("Iteration===>", cities[i])
+//  }
+
+// question 7;
+
+// var cities =["Multan", "Faislabad", "Islamabad", "Naran", "sukkur"];
+
+
+//  for (var i =0; i <=cities.length; i++) {
+//      console.log("Iteration===>", cities[i]);
+
+//      if(cities[i]== "Islamabad"){
+//         alert("found");
+//         break;
+//     } else{
+//          alert("not found")
+//      }
+//  }
+
+// question 9;
+
+// var are = [24,53,78,91,12];
+//  var min = are[0];
+
+//  for (var i = 0; i <are.length; i++){
+//     if(are[i] <min){
+//         min = are[i]
+//      }
+//  }
+
+//  console.log(min);
+
+//question 8
+
+// var are = [24,53,78,91,12];
+//  var max = are[0];
+
+//  for (var i = 0; i <are.length; i++){
+//     if(are[i]>max){
+//         max = are[i]
+//      }
+//  }
+
+//  console.log(max);
+
+// question 10;
+
+// var table = 5
+
+//  for (var i = 1; i <= 20; i++){
+//      console.log("Table===>", table + "x" + i + "=" + table * i);
+//  }
+
+// question 2;
+
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,1],
+//     [2,1,0,1]
+// ];
+
+// console.log(matrix)
+
+// question 1;
+
+// var arr = [
+//   [],
+//   [],
+//   []
+// ];
+
+// console.log(arr)
+
+// question 4;
+
+//var userinput = +prompt("Enter your number")
+
+ //for (var i = 1; i <= 15; i++){
+    // console.log( userinput + "x" + i + "=" + userinput * i);
+ //}
+
+
+// question 6;
+
+
+//var i;
+
+ //for ( i= 1; i<=10; i++) {
+  //document.write( i+ ",")
+    
+ //}
+
+
